2c9afda819379f2e776db9dbbef1e669c47a74c8
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-11-16 19:04:47 +0430

    Add action component

---
+++ src/components/Actions.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Actions() {
+  return (
+    <div>
+      <button type="button">Comment</button>
+      <button type="button">Remove</button>
+      <button type="button">Edit</button>
+    </div>
+  );
+}
