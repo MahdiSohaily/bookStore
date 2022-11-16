b48658e53029c754b7984b91ded1f765e8cbe0c6
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-11-16 22:14:00 +0430

    Add script for categories actions

--- src/pages/Categories.jsx
+++ src/pages/Categories.jsx
@@ -1,5 +1,18 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { checkStatus } from '../redux/categories/categories';
 
 export default function Categories() {
-  return <button type="button">Check status</button>;
+  const data = useSelector((state) => state.categories);
+  const dispatch = useDispatch();
+
+  const handleStatus = () => dispatch(checkStatus('Under construction'));
+  return (
+    <>
+      <h2>{data}</h2>
+      <button type="button" onClick={handleStatus}>
+        Check status
+      </button>
+    </>
+  );
 }
