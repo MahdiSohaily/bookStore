2675e62dd6a1577a641b79997a86dd7ead9e6840
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-11-17 20:49:51 +0430

    Add Enhancers

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,12 +1,16 @@
 import { createStore, combineReducers } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import bookReducer from './books/books';
 import categoriesReducer from './categories/categories';
+
+const composeEnhancers = composeWithDevTools();
 
 const store = createStore(
   combineReducers({
     books: bookReducer,
     categories: categoriesReducer,
   }),
+  composeEnhancers,
 );
 
 export default store;
